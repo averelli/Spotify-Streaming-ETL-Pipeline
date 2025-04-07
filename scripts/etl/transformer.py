e61108cda6f8aaa4fe852e5dfb1177399869d259
averelli <beloglazov.roman06@gmail.com>
2025-04-07 18:35:11 +0300

    Add fact table handing
    Also:
    - Make a run method to run everything
    - Minor code addition for logging
    - Add docstrings

--- scripts/etl/transformer.py
+++ scripts/etl/transformer.py
@@ -82,7 +82,7 @@ class DataTransformer:
             raw_podcast (dict): Raw podcast JSON from the staging layer.
         
         Returns:
-            tuple or None: (spotify_podcast_uri, show_name, description, cover_art_url),
+            tuple or None: (spotify_podcast_uri, podcast_name, description, podcast_cover_art_url),
             or None if an error occurs.
         """
         try:
@@ -107,7 +107,7 @@ class DataTransformer:
             raw_episode (dict): Raw episode JSON from the staging layer.
         
         Returns:
-            tuple or None: (spotify_episode_uri, duration_ms, duration_sec, show_name, spotify_show_uri, release_date), 
+            tuple or None: (spotify_episode_uri, duration_ms, duration_sec, podcast_name, spotify_podcast_uri, release_date), 
             or None if an error occurs.
         """
         try:
@@ -158,7 +158,14 @@ class DataTransformer:
             
         return clean_date
         
-    def process_staged_batches(self, item_type:str):
+    def process_staged_batches(self, item_type:str) -> float:
+        """
+        Transforms and loads staged Spotify dimension data into the core dimension tables.        
+        Args:
+            item_type (str): The type of item to process. Accepted values are "tracks", "artists", "podcasts", or "episodes".            
+        Returns:
+            float: The total time taken to process all batches for the given item type.
+        """
         # metrics
         time_start = time.perf_counter()
         total_items_count = 0
@@ -182,20 +189,24 @@ class DataTransformer:
 
         elif item_type == "podcasts":
             cleaning_func = self._clean_podcast
-            columns = ["spotify_podcast_uri", "show_name", "description", "cover_art_url"]
+            columns = ["spotify_podcast_uri", "podcast_name", "description", "podcast_cover_art_url"]
             target_table = "core.dim_podcast"
 
         elif item_type == "episodes":
             cleaning_func = self._clean_episode
-            columns = ["spotify_episode_uri", "duration_ms", "duration_sec", "show_name", "spotify_show_uri", "release_date"]
+            columns = ["spotify_episode_uri", "duration_ms", "duration_sec", "podcast_name", "spotify_podcast_uri", "release_date"]
             target_table = "core.dim_episode"
 
+        else:
+            self.logger.error(f"Invalid item type passed. Expected 'tracks', 'artists', 'episodes' or 'podcasts', got: {item_type}")
+            raise ValueError(f"Invalid item type passed. Expected 'tracks', 'artists', 'episodes' or 'podcasts', got: {item_type}")
     
         # query the staging layer for raw data and IDs
         staged_items = self.db.execute_query(f"SELECT record_id, raw_data FROM staging.spotify_{item_type}_data WHERE is_processed = FALSE;")
         if not staged_items:
             self.logger.warning("No unprocessed staged items found")
-            return
+            total_time = time.perf_counter() - time_start
+            return total_time
 
         for i in range(0, len(staged_items), self.BATCH_SIZE):
             batch_number = i // self.BATCH_SIZE + 1
@@ -237,13 +248,137 @@ class DataTransformer:
         self.logger.info(f"All batches processed successfully in {total_time} seconds. Inserted {total_items_count} {item_type}")
         return total_time
 
-    def run(self):
+    def insert_core_facts(self, item_type:str) -> float:
+        """
+        Loads new fact records into the core fact table for the specified item type.
         
-        item_types = ["tracks", "artists", "podcasts", "episodes"]
-        total_time = 0.0
+        For item_type "track" or "podcast", this function executes an INSERT query that
+        joins the staging table with the appropriate dimension tables to generate fully transformed fact rows. It uses a delta load
+        approach based on the maximum timestamp already present in the fact table.        
+        Args:
+            item_type (str): The type of fact to load, either "track" or "podcast".            
+        Returns:
+            float: The time taken to execute the insertion.
+        Raises:
+            ValueError: If an invalid item type is provided.
+        """
+        # metrics
+        time_start = time.perf_counter()
+        row_count = 0
 
-        for item_type in item_types:
+        self.logger.info(f"Started inserting data into fact_{item_type}s_history")
+
+        if item_type == "track":
+            query = """
+            INSERT INTO core.fact_tracks_history (
+            ts_msk, date_fk, time_fk, ms_played, sec_played, 
+            track_fk, artist_fk, reason_start_fk, reason_end_fk, 
+            shuffle, session_length_category, offline, offline_timestamp
+            )
+            SELECT
+                s.ts AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Moscow' AS ts_msk,
+                d.date_id,
+                t.time_id,
+                s.ms_played,
+                s.ms_played / 1000,
+                dt.track_id,
+                da.artist_id,
+                rs.reason_id,
+                re.reason_id,
+                s.shuffle,
+                CASE
+                    WHEN s.ms_played < 10000 THEN 'skipped'
+                    WHEN ABS(s.ms_played - dt.duration_ms) <= 15000 THEN 'full song'
+                    ELSE 'partial'
+                END AS session_length_category,
+                s.offline,
+                s.offline_timestamp
+            FROM staging.streaming_history s
+            LEFT JOIN core.dim_date d ON d.date = (s.ts AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Moscow')::date
+            LEFT JOIN core.dim_time t ON t.time = date_trunc('minute', s.ts AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Moscow')::time
+            LEFT JOIN core.dim_track dt ON s.spotify_track_uri = dt.spotify_track_uri
+            LEFT JOIN core.dim_artist da ON dt.spotify_artist_uri = da.spotify_artist_uri
+            LEFT JOIN core.dim_reason rs ON s.reason_start = rs.reason_type AND rs.reason_group = 'start'
+            LEFT JOIN core.dim_reason re ON s.reason_end = re.reason_type AND re.reason_group = 'end'
+            WHERE 
+                s.spotify_track_uri IS NOT NULL 
+                AND 
+                s.ts > (
+                SELECT COALESCE(MAX(ts_msk AT TIME ZONE 'Europe/Moscow' AT TIME ZONE 'UTC'), '1900-01-01'::timestamp)
+                FROM core.fact_tracks_history
+            );
+            """
+        elif item_type == "podcast":
+            query = """
+            INSERT INTO core.fact_podcasts_history (ts_msk, date_fk, time_fk, sec_played, episode_fk, podcast_fk, reason_start_fk, reason_end_fk)
+            SELECT
+                s.ts AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Moscow' AS ts_msk,
+                d.date_id,
+                t.time_id,
+                s.ms_played / 1000,
+                COALESCE(de.episode_id, 0),
+                COALESCE(dp.podcast_id, 0),
+                rs.reason_id,
+                re.reason_id
+            FROM staging.streaming_history s
+            LEFT JOIN core.dim_date d ON d.date = (s.ts AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Moscow')::date
+            LEFT JOIN core.dim_time t ON t.time = date_trunc('minute', s.ts AT TIME ZONE 'UTC' AT TIME ZONE 'Europe/Moscow')::time
+            LEFT JOIN core.dim_episode de ON s.spotify_episode_uri = de.spotify_episode_uri
+            LEFT JOIN core.dim_podcast dp ON de.spotify_podcast_uri = dp.spotify_podcast_uri
+            LEFT JOIN core.dim_reason rs ON s.reason_start = rs.reason_type AND rs.reason_group = 'start'
+            LEFT JOIN core.dim_reason re ON s.reason_end = re.reason_type AND re.reason_group = 'end'
+            WHERE
+                s.spotify_episode_uri IS NOT NULL
+                AND
+                s.ts > (
+                    SELECT COALESCE(MAX(ts_msk AT TIME ZONE 'Europe/Moscow' AT TIME ZONE 'UTC'), '1900-01-01'::timestamp)
+                    FROM core.fact_podcasts_history
+            );
+            """
+        else:
+            self.logger.error(f"Invalid item type passed. Expected 'track' or 'podcast', got: {item_type}")
+            raise ValueError(f"Invalid item type passed. Expected 'track' or 'podcast', got: {item_type}")
+        
+
+        try:
+            self.db.execute_query(query)
+
+            total_time = time.perf_counter() - time_start
+            row_count = self.db.cursor.rowcount
+            self.logger.info(f"Inserted {row_count} rows into fact_tracks_history in {total_time} seconds")
+
+            return total_time
+        
+        except Exception as e:
+            self.logger.error(f"Error while inserting data into fact_tracks_history: {e}")
+            raise
+
+    def run(self) -> int:
+        """
+        Orchestrates the full data transformation and loading process.
+        
+        This method sequentially processes each dimension type ("tracks", "artists", "podcasts", "episodes")
+        from the staging tables into the corresponding core dimension tables, and then loads the fact tables
+        for "track" and "podcast" data. It logs processing times and metrics for each stage.
+        
+        Returns:
+            int: Total time for the entire process.
+        """
+        self.logger.info("Started running data transformation and loading")
+        
+        dim_item_types = ["tracks", "artists", "podcasts", "episodes"]
+        fact_item_types = ["track", "podcast"]
+
+        total_time = 0
+
+        for item_type in dim_item_types:
             process_time = self.process_staged_batches(item_type)
             total_time += process_time
 
-        self.logger.info(f"All items processed in {total_time} seconds")
+        for item_type in fact_item_types:
+            process_time = self.insert_core_facts(item_type)
+            total_time += process_time
+
+        self.logger.info(f"Finihsed running data transformation and loading in {total_time} seconds")
+
+        return total_time
