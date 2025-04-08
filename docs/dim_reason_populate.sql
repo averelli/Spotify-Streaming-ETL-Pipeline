f276a40cccbdee48e1cd4c18d2e3db10d83ac953
averelli <beloglazov.roman06@gmail.com>
2025-04-08 14:31:42 +0300

    Move to a separate folder

--- docs/dim_reason_populate.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO core.dim_reason (reason_type, reason_group)
-SELECT DISTINCT reason_start AS reason_type, 'start' AS reason_group FROM staging.streaming_history
-UNION ALL
-SELECT DISTINCT reason_end, 'end' AS reason_group FROM staging.streaming_history
-ON CONFLICT DO NOTHING;
