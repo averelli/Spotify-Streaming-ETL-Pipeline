ab38c396dfa5363f499d2171efb6497b6c0ed562
averelli <beloglazov.roman06@gmail.com>
2025-07-29 22:45:23 +0300

    chore: update dm schema

--- docs/sql/dm_ddl.sql
+++ docs/sql/dm_ddl.sql
@@ -14,14 +14,32 @@ create table if not exists dm.parent_tracks
     foreign key (child_id) references core.dim_track
 );
 
+-- yearly aggregations
+create or replace view dm.yearly_agg as
+select
+    year,
+    make_date(year, 01, 01) year_start,
+    round(sum(sec_played) / 3600.0, 1) hours_listened,
+    count(stream_id) total_streams_sessions,
+    count(case when sec_played > 10 then stream_id end) nonskip_sessions,
+    round(sum(percent_played) / 100) total_estimated_streams,
+    count(distinct track_fk) distinct_tracks,
+    count(distinct artist_fk) distinct_artists
+from core.fact_tracks_history fh
+    join core.dim_date dd on fh.date_fk = dd.date_id
+group by year
+order by year desc;
+
 -- monthly aggregations
 create or replace view dm.monthly_agg as
 select
     year,
     month_num,
+    make_date(year, month_num, 01) month_start,
     round(sum(sec_played) / 3600.0, 1) hours_listened,
-    count(stream_id) total_streams_sessions,
-    count(case when sec_played > 10 then stream_id end) total_valid_streams,
+    count(stream_id) total_streams,
+    count(case when sec_played > 10 then stream_id end) nonskip_streams,
+    round(sum(percent_played) / 100) total_estimated_streams,
     count(distinct track_fk) distinct_tracks,
     count(distinct artist_fk) distinct_artists
 from core.fact_tracks_history fh
@@ -31,7 +49,7 @@ order by year desc, month_num desc;
 
 -- albums function
 create or replace function dm.top_albums(filter_year int default null, filter_month int default null, return_limit int default 100)
-returns table(album varchar, artist varchar, hours_played numeric, times_played int, full_sum_streams int, full_real_streams int)
+returns table(album varchar, artist varchar, hours_played numeric, raw_play_count int, estimated_full_streams int, full_real_streams int, cover_art text)
 language plpgsql
 as $$
     begin
@@ -39,10 +57,11 @@ as $$
             select
                 coalesce(p.parent_album_name, dt.album_name) album,
                 dt.artist_name album_artist,
-                round(sum(sec_played) / 3600, 1) hours_played,
+                round(sum(sec_played) / 3600.0, 1) hours_played,
                 count(stream_id)::int times_played,
-                round(sum(percent_played) / 100)::int full_sum_streams,
-                count(case when percent_played = 100 then stream_id end)::int full_real_streams
+                round(sum(percent_played) / 100)::int estimated_streams,
+                count(case when percent_played = 100 then stream_id end)::int full_real_streams,
+                max(dt.cover_art_url) cover_art -- since we don't have an album dim random track cover art should be good enough
             from core.fact_tracks_history h
                 join core.dim_track dt on dt.track_id = h.track_fk
                 join core.dim_date dd on h.date_fk = dd.date_id
@@ -57,7 +76,7 @@ $$;
 
 -- tracks function
 create or replace function dm.top_tracks(filter_year int default null, filter_month int default null, return_limit int default 100)
-returns table(track varchar, artist varchar, hours_played numeric, times_played int, full_sum_streams int, full_real_streams int)
+returns table(track varchar, artist varchar, hours_played numeric, raw_play_count int, estimated_full_streams int, full_real_streams int, cover_art text)
 language plpgsql
 as $$
     begin
@@ -65,10 +84,11 @@ as $$
             select
                 coalesce(p.parent_track_title, dt.track_title) track,
                 dt.artist_name track_artist,
-                round(sum(sec_played) / 3600, 1) hours_played,
+                round(sum(sec_played) / 3600.0, 1) hours_played,
                 count(stream_id)::int times_played,
-                round(sum(percent_played) / 100)::int full_sum_streams,
-                count(case when percent_played = 100 then stream_id end)::int full_real_streams
+                round(sum(percent_played) / 100)::int estimated_streams,
+                count(case when percent_played = 100 then stream_id end)::int full_real_streams,
+                max(dt.cover_art_url) cover_art
             from core.fact_tracks_history h
                 join core.dim_track dt on dt.track_id = h.track_fk
                 join core.dim_date dd on h.date_fk = dd.date_id
@@ -83,20 +103,21 @@ $$;
 
 -- artists function
 create or replace function dm.top_artists(filter_year int default null, filter_month int default null, return_limit int default 100)
-returns table(artist varchar, hours_played numeric, times_played int, full_sum_streams int, full_real_streams int)
+returns table(artist text, hours_played numeric, raw_play_count int, estimated_full_streams int, full_real_streams int, cover_art text)
 language plpgsql
 as $$
     begin
         return query
             select
-                dt.artist_name artist,
-                round(sum(sec_played) / 3600, 1) hours_played,
+                da.artist_name artist,
+                round(sum(sec_played) / 3600.0, 1) hours_played,
                 count(stream_id)::int times_played,
                 round(sum(percent_played) / 100)::int full_sum_streams,
-                count(case when percent_played = 100 then stream_id end)::int full_real_streams
+                count(case when percent_played = 100 then stream_id end)::int full_real_streams,
+                max(da.cover_art_url) cover_art
             from core.fact_tracks_history h
-                join core.dim_track dt on dt.track_id = h.track_fk
                 join core.dim_date dd on h.date_fk = dd.date_id
+                join core.dim_artist da on h.artist_fk = da.artist_id
             where (filter_year is null or dd.year = filter_year)
                 and (filter_month is null or (filter_year is not null and dd.month_num = filter_month))
             group by artist
