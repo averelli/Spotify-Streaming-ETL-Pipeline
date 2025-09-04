5d8ca8637d8839fcf1fc6b05cda9340412970fc2
averelli <beloglazov.roman06@gmail.com>
2025-09-04 20:09:51 +0300

    chore: add album stats function and artist filters to other functions

--- docs/sql/dm_ddl.sql
+++ docs/sql/dm_ddl.sql
@@ -60,7 +60,7 @@ from core.fact_tracks_history fh
     join core.dim_date dd on fh.date_fk = dd.date_id;
 
 -- albums function
-create or replace function dm.top_albums(filter_year int default null, filter_month int default null, return_limit int default 100)
+create or replace function dm.top_albums(filter_year int default null, filter_month int default null, return_limit int default 100, filter_artist varchar default null)
 returns table(album varchar, artist varchar, hours_played numeric, raw_play_count int, estimated_full_streams int, full_real_streams int, cover_art text)
 language plpgsql
 as $$
@@ -71,7 +71,7 @@ as $$
                 dt.artist_name album_artist,
                 round(sum(sec_played) / 3600.0, 1) hours_played,
                 count(stream_id)::int times_played,
-                round(sum(percent_played) / 100)::int estimated_streams,
+                round(sum(percent_played) / 100)::int full_sum_streams,
                 count(case when percent_played = 100 then stream_id end)::int full_real_streams,
                 max(dt.cover_art_url) cover_art -- since we don't have an album dim random track cover art should be good enough
             from core.fact_tracks_history h
@@ -80,6 +80,7 @@ as $$
                 left join dm.parent_tracks p on h.track_fk = p.child_id
             where (filter_year is null or dd.year = filter_year)
                 and (filter_month is null or (filter_year is not null and dd.month_num = filter_month))
+                and (filter_artist is null or dt.artist_name = filter_artist)
             group by album, album_artist
             order by hours_played desc
             limit return_limit;
@@ -87,7 +88,7 @@ as $$
 $$;
 
 -- tracks function
-create or replace function dm.top_tracks(filter_year int default null, filter_month int default null, return_limit int default 100)
+create or replace function dm.top_tracks(filter_year int default null, filter_month int default null, filter_artist varchar default null, return_limit int default 100)
 returns table(track varchar, artist varchar, hours_played numeric, raw_play_count int, estimated_full_streams int, full_real_streams int, cover_art text)
 language plpgsql
 as $$
@@ -98,7 +99,7 @@ as $$
                 dt.artist_name track_artist,
                 round(sum(sec_played) / 3600.0, 1) hours_played,
                 count(stream_id)::int times_played,
-                round(sum(percent_played) / 100)::int estimated_streams,
+                round(sum(percent_played) / 100)::int full_sum_streams,
                 count(case when percent_played = 100 then stream_id end)::int full_real_streams,
                 max(dt.cover_art_url) cover_art
             from core.fact_tracks_history h
@@ -107,6 +108,7 @@ as $$
                 left join dm.parent_tracks p on h.track_fk = p.child_id
             where (filter_year is null or dd.year = filter_year)
                 and (filter_month is null or (filter_year is not null and dd.month_num = filter_month))
+                and (filter_artist is null or dt.artist_name = filter_artist)
             group by track, track_artist
             order by hours_played desc
             limit return_limit;
@@ -135,5 +137,26 @@ as $$
             group by artist
             order by hours_played desc
             limit return_limit;
+    end;
+$$;
+
+-- top songs in an album
+create or replace function dm.album_stats(filter_album varchar, filter_artist varchar)
+returns table(track varchar, min_listened numeric, total_estimated_streams int)
+language plpgsql as
+$$
+    begin
+        return query
+            select
+                coalesce(p.parent_track_title, dt.track_title) as track,
+                round(sum(sec_played) / 60.0, 1) as min_listened,
+                round(sum(percent_played) / 100)::int as total_estimated_streams
+            from core.fact_tracks_history th
+            join core.dim_track dt on th.track_fk = dt.track_id
+            left join dm.parent_tracks p on dt.track_id = p.child_id
+            where coalesce(p.parent_album_name, dt.album_name) = filter_album
+                and coalesce(p.artist, dt.artist_name) = filter_artist
+            group by coalesce(p.parent_track_title, dt.track_title)
+            order by min_listened desc;
     end;
 $$;
